fd01673012941060921c86fd0413288d0e5060b4
karen chan <karen@karen-chan.com>
2015-03-30 02:30:48 +0100

    Improve email message display page
    - Display all email headers in a table
    - Link to view original email
    - Display the text and html parts of the email

--- email_tester.py
+++ email_tester.py
@@ -45,6 +45,69 @@ class MailHandler(webapp2.RequestHandler):
 
 class Message(webapp2.RequestHandler):
     def get(self, msgkey):
+        mail = email.message_from_string(Email.get(msgkey).message)
+        self.response.headers['Content-Type'] = 'text/html'
+        self.response.out.write(u'''
+<html>
+    <head>
+        <title>{subject}</title>
+        <style type="text/css">
+            td:first-child {{
+                text-align: right;
+            }}
+            td {{
+                font-size: 80%;
+                vertical-align: top;
+            }}
+            pre {{
+                white-space: pre-wrap;
+                background-color: #EEE;
+                padding: 15px;
+            }}
+        </style>
+    </head>
+    <body>
+        <p><a href="/plain-text/{key}">View original mail</a></p>
+        '''.format(subject=cgi.escape(mail.get('Subject')), key=msgkey))
+
+        self.response.out.write(u'<table>')
+        for key in mail.keys():
+            self.response.out.write(
+                u'<tr><td>{key}:</td><td>{value}</td></tr>'.format(
+                    key=cgi.escape(key), value=cgi.escape(mail.get(key))))
+        self.response.out.write(u'</table>')
+
+        text, html = self._get_content(mail)
+
+        self.response.out.write(u'<pre>{}</pre>'.format(cgi.escape(text)))
+        self.response.out.write(u'<pre>{}</pre>'.format(cgi.escape(html)))
+        self.response.out.write(u'</body></html>')
+
+    def _get_content(self, mail):
+        text = ''
+        html = ''
+        if mail.is_multipart():
+            for part in mail.get_payload():
+                if part.get_content_type() == 'multipart/alternative':
+                    return self._get_content(part)
+
+                if part.get_content_charset() is None:
+                    continue
+                charset = part.get_content_charset()
+
+                _text = part.get_payload(decode=True).decode(str(charset))
+                if part.get_content_type() == 'text/plain':
+                    text = _text
+                elif part.get_content_type() == 'text/html':
+                    html = _text
+        else:
+            text = mail.get_payload(decode=True).decode(
+                str(mail.get_content_charset()))
+        return text, html
+
+
+class PlainText(webapp2.RequestHandler):
+    def get(self, msgkey):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.out.write(Email.get(msgkey).message)
 
@@ -104,6 +167,7 @@ app = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/_ah/mail/(.+)', MailHandler),
     ('/message/(.+)', Message),
+    ('/plain-text/(.+)', PlainText),
     ('/inbox/(.+)', Inbox),
     ('/delete/(.+)', Delete),
     ], debug=True)
